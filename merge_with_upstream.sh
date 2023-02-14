9f45afc92424d98d6cb5c3860108552d301f4030
Vines <34955859+vinesmsuic@users.noreply.github.com>
2023-02-14 16:16:24 -0500

    Update merge_with_upstream.sh

--- merge_with_upstream.sh
+++ merge_with_upstream.sh
@@ -2,5 +2,5 @@
 git checkout "$1"
 git remote add upstream git@github.com:UWVerse/uwconnect-frontend.git || true
 git fetch upstream
-git merge upstream/main --no-edit
+git merge upstream/"$1" --no-edit
 git push
