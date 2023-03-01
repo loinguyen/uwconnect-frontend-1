20bde55acf0a9f5ac4de5170d9b2ab796928c226
vinesmsuic <34955859+vinesmsuic@users.noreply.github.com>
2023-03-01 16:53:16 -0500

    changing scripts

--- create_branch_from_dev.sh
+++ create_branch_from_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 git remote add upstream git@github.com:UWVerse/uwconnect-frontend.git || true
 git fetch upstream
-git checkout -b "$1" upstream/main
+git checkout -b "$1" upstream/dev
 git push --set-upstream origin "$1"
--- merge_with_upstream.sh
+++ merge_with_upstream.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
+# Usage: ./merge_with_upstream.sh <ur checkout branch> <ur merge update remote branch>
 git checkout "$1"
 git remote add upstream git@github.com:UWVerse/uwconnect-frontend.git || true
 git fetch upstream
-git merge upstream/main --no-edit
+git merge upstream/"$2" --no-edit
 git push
