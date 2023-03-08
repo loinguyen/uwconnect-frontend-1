2a9b4f1da9a5222ba1eed6f88f7ef23b5fce02ee
Qixin <mersenyip0130@gmail.com>
2023-03-08 19:05:13 +0000

    prototype

--- src/route/home/Home.jsx
+++ src/route/home/Home.jsx
@@ -1,42 +1,63 @@
-import React, { useState } from 'react'
+import { CometChat } from "@cometchat-pro/chat";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { CometChatUI } from "../../cometchat-pro-react-ui-kit/CometChatWorkspace/src";
 
-import { ChatEngine, getOrCreateChat } from 'react-chat-engine'
 
 const Home = () => {
-	const [username, setUsername] = useState('')
+    const [uid, setUid] = useState(null);
 
-	function createDirectChat(creds) {
-		getOrCreateChat(
-			creds,
-			{ is_direct_chat: true, usernames: [username] },
-			() => setUsername('')
-		)
-	}
+    const appID = "2341304f6c94b6b0";
+    const region = "us";
+    const authKey = "62a7f4ed9a36e08fe7c0c71c5435fb4fea1e452b";
 
-	function renderChatForm(creds) {
-		return (
-			<div>
-				<input 
-					placeholder='Username' 
-					value={username} 
-					onChange={(e) => setUsername(e.target.value)} 
-				/>
-				<button onClick={() => createDirectChat(creds)}>
-					Create
-				</button>
-			</div>
-		)
-	}
+    useEffect(() =>{
+        fetch('http://localhost:5000/user/who', { credentials: 'include' })
+        .then(response => {
+            if (response.status === 403) {
+                window.location.href = '/';
+            }
+            return response.json()
+        })
+        .then(response => {
 
-	return (
-		<ChatEngine
-			height='100vh'
-            projectID='65fefe97-fdba-4815-bc12-c54a515c4377'
-            userName='qixin'
-            userSecret='caonima'
-            renderNewChatForm={(creds) => renderChatForm(creds)}
-		/>
-	)
-}
+            console.log("trying to init cometchat", response.email.split('@')[0])
+
+            const appSetting = new CometChat.AppSettingsBuilder()
+                .subscribePresenceForAllUsers()
+                .setRegion(region)
+                .build();
+            CometChat.init(appID, appSetting).then(
+                () => {
+                    console.log("Initialization completed successfully");
+                    CometChat.login(response.email.split('@')[0], authKey);
+                },
+                (error) => {
+                    console.log("Initialization failed with error:", error);
+                    // Check the reason for error and take appropriate action.
+                }
+            ).then(
+                (user) => {
+                    console.log("Login Successful:", { user });
+                },
+                (error) => {
+                    console.log("Login failed with exception:", { error });
+                }
+            )
+
+            setUid(response.email.split('@')[0]);
+
+        });
+    })
+    
+    return (
+        <div style={{ width: "100wh", height: "100vh" }}>
+            {/* {console.log('rerendered')} */}
+            {uid && <CometChatUI />}
+        </div>
+    );
+
+    }
+
 
 export default Home;
--- src/route/login/loginForm.jsx
+++ src/route/login/loginForm.jsx
@@ -21,7 +21,8 @@ function GetLoginForm() {
         
         if (EmailError == "") {
             // fetch(process.env.REACT_APP_API_LINK + '/user/validate', {
-            fetch('http://127.0.0.1:5000/user/validate', {
+            fetch('http://localhost:5000/user/validate', {
+            credentials: 'include',
             method: 'POST',
             headers: {
             'Accept': 'application/json',
@@ -40,7 +41,7 @@ function GetLoginForm() {
 
     function CheckLoginStatus() {
         if (LoginStatus == true) {
-            navigate('/home',{state:{name:Email}});
+            window.location.href = '/home';
         } 
     }
     
