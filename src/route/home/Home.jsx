79c09021c5308c3ad17eacc499b95ec38af319e7
lmnguyen <loiminh.nguyen@uwaterloo.ca>
2023-03-26 11:29:45 -0400

    ECE-53: Update bottom nav bar and re styling the header user avatar

--- src/route/home/Home.jsx
+++ src/route/home/Home.jsx
@@ -3,11 +3,12 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { CometChatUI, CometChatMessages, CometChatConversationList, CometChatUserList } from "../../cometchat-pro-react-ui-kit/CometChatWorkspace/src";
 import { Button } from "antd";
-import { WeiboCircleOutlined } from '@ant-design/icons'
+import { WifiOutlined } from '@ant-design/icons'
 import GetRecommendation from '../../components/Recommendation'
 import styles from'../../styles/home.css'
 import { useSelector, useDispatch } from "react-redux";
 import { setUser } from "../../redux/profile/profileSlice";
+import BottomNavBar from "../../components/BottomNavBar";
 
 const authKey = process.env.REACT_APP_COMETCHAT_AUTH_KEY;
 
@@ -18,6 +19,7 @@ const Home = () => {
     const [conversationIdMap, setConversationIdMap] = useState(new Map());
     const [openConnection, setOpenConnection] = useState(false);
     const [renderCometChat, setRenderCometChat] = useState(false);
+    const [selectedMessageTab, setSelectedMessageTab] = useState('message');
     const email = useSelector((state) => state.auth.email);
     
     // let appID = process.env.REACT_APP_COMETCHAT_APPID;
@@ -63,19 +65,30 @@ const Home = () => {
         setOpenConnection(!openConnection)
     }
 
+    const tabSelected = (tabName) => {
+        if (selectedMessageTab !== tabName) {
+            setSelectedMessageTab(tabName)
+        }
+    }
+
     return (
         <div className="position-fixed" style={{inset: '0', top: '5vh'}}>
             {renderCometChat && 
                 <div className="col-12 d-flex" style={{height: '95vh'}}>
-                    <div className="col-3 h-100 align-self-end" style={{backgroundColor: 'rgba(255,255,255,0.1)'}}>
-                        <div className="m-auto d-flex align-item-center pb-1" style={{height: '80px'}}>
-                            <Button type="primary" className="connect-me-button h-100 m-auto w-100" icon={<WeiboCircleOutlined style={{ fontSize: '150%'}} />} 
+                    <div className="col-3 h-100 align-self-end d-flex flex-column" style={{backgroundColor: 'rgba(255,255,255,0.1)', alignItems: 'stretch'}}>
+                        <div className="d-flex align-item-center pb-1" style={{height: '80px'}}>
+                            <Button type="primary" className="connect-me-button h-100 m-auto w-100" icon={<WifiOutlined style={{ fontSize: '150%'}} />} 
                             onClick={openConnectionPage}>
                                     Connect Me
                             </Button>
                         </div>
-                        {/* <CometChatConversationList onItemClick={handleConversationSelect}/> */}
-                        <CometChatUserList friendsOnly={true} onItemClick={handleConversationSelect}/>
+                        <div className="row m-0" style={{flex: 1}}>
+                            {selectedMessageTab === 'message' && <CometChatConversationList onItemClick={handleConversationSelect}/>}
+                            {selectedMessageTab === 'friends' && <CometChatUserList friendsOnly={true} onItemClick={handleConversationSelect}/>}
+                        </div>
+                        <div className="row d-flex m-0">
+                            <BottomNavBar tabUpdateHanlder={tabSelected} />
+                        </div>
                     </div>
                     { !openConnection && renderCometChat &&
                         [...conversationIdMap.keys()].map(k => (
